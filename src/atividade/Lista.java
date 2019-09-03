e4642a0acbb7bda38dceaecd29c5b47ae950d762
Renata Cardoso Chagas <renata.cardoso@academico.ifrn.edu.br>
2019-09-03 15:58:57 -0300

    first commit

---
+++ src/atividade/Lista.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade;
+
+/**
+ *
+ * @author 20171148060029
+ */
+public class Lista {
+
+    private int tamMax;
+    private int indice;
+    private Pessoa lista;
+
+    void inserir(Pessoa pessoa) {
+    }
+
+    void inserir(int posicao, Pessoa pessoa) {
+    }
+
+    int recuperar(int posicao) {
+        return 0;
+    }
+
+    void remover(int posicao) {
+
+    }
+
+    Boolean contem(Pessoa pessoa) {
+        return null;
+    }
+
+    int tamanho() {
+        return 0;
+    }
+
+    void listarTodos() {
+
+    }
+}
---
+++ src/atividade/Pessoa.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade;
+
+/**
+ *
+ * @author 20171148060029
+ */
+public class Pessoa {
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+   
+    public void Pessoa() {
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", email=" + email + ", telefone=" + telefone + '}';
+    }
+
+}
